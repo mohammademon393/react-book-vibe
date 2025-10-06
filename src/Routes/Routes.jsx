1701fd2658b95227423c227141ff0ce47e6b20aa
mohammademon393 <mdemon357ep@gmail.com>
2025-10-06 17:37:28 +0600

    create a home jsx component

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -2,11 +2,20 @@ import React from 'react';
 import { createBrowserRouter } from "react-router";
 import Roots from '../Pages/Roots/Roots';
 import ErrorPage from '../Pages/ErrorPage/ErrorPage';
+import Home from '../Pages/Home/Home';
 
 export const router = createBrowserRouter([
   {
     path: "/",
     Component: Roots,
-    errorElement: <ErrorPage></ErrorPage>
+    errorElement: <ErrorPage></ErrorPage>,
+
+    children:[
+        {
+            index: true,
+            path:'/',
+            Component: Home,
+        }
+    ]
   },
 ]);
