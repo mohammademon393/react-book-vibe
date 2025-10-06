2cd04682cbb31423fef2777d9b868ec54d3f1c26
mohammademon393 <mdemon357ep@gmail.com>
2025-10-06 20:30:11 +0600

    banner design and create books json data

--- src/Component/Header/Banner/Banner.jsx
+++ src/Component/Header/Banner/Banner.jsx
@@ -3,12 +3,12 @@ import books from '../../../assets/books.jpg';
 
 const Banner = () => {
     return (
-      <div className="bg-[#1313130d] rounded-2xl flex justify-between my-5 items-center p-5">
+      <div className="bg-[#1313130d] rounded-xl justify-center md:flex md:justify-around my-5 items-center p-10">
         <div>
-          <h1>Books to freshen up your bookshelf</h1>
-          <button className="bg-[#23be0a] btn">View The List</button>
+          <h1 className='text-[56px] font-bold'>Books to freshen up <br /> your bookshelf</h1>
+          <button className="bg-[#23be0a] text-white mt-5 btn">View The List</button>
         </div>
-        <img src={books} alt="" />
+        <img src={books} width={300} className=' rounded-xl' alt="img comeing soon" />
       </div>
     );
 };
