1edb4a54bd80f3630aee60c5b586f553e3944b31
mohammademon393 <mdemon357ep@gmail.com>
2025-10-06 17:58:21 +0600

    A awesome Footer added

--- src/Component/Footer/Footer.jsx
+++ src/Component/Footer/Footer.jsx
@@ -2,9 +2,34 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
-            <h1>Ami footer</h1>
-        </div>
+      <footer className="footer sm:footer-horizontal bg-neutral text-neutral-content p-10 flex justify-between">
+        <nav>
+          <h6 className="footer-title">Services</h6>
+          <a className="link link-hover">Branding</a>
+          <a className="link link-hover">Design</a>
+          <a className="link link-hover">Marketing</a>
+          <a className="link link-hover">Advertisement</a>
+        </nav>
+        <nav>
+          <h6 className="footer-title">Company</h6>
+          <a className="link link-hover">About us</a>
+          <a className="link link-hover">Contact</a>
+          <a className="link link-hover">Jobs</a>
+          <a className="link link-hover">Press kit</a>
+        </nav>
+        <nav>
+          <h6 className="footer-title">Legal</h6>
+          <a className="link link-hover">Terms of use</a>
+          <a className="link link-hover">Privacy policy</a>
+          <a className="link link-hover">Cookie policy</a>
+        </nav>
+        <nav>
+          <h6 className="footer-title">Contect</h6>
+          <a className="link link-hover">E-mail: emon33@mail.com</a>
+          <a className="link link-hover">Facebook: www.facebook.com</a>
+          <a className="link link-hover">Phone: 01812-345678</a>
+        </nav>
+      </footer>
     );
 };
 
