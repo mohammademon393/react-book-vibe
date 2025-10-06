db120cdafe8236aae3a1d47bf30804dbc9c4f304
mohammademon393 <mdemon357ep@gmail.com>
2025-10-06 17:15:39 +0600

    create a routes component and export router outside of file

--- src/main.jsx
+++ src/main.jsx
@@ -2,19 +2,14 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
-import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
+import { router } from './Routes/Routes.jsx';
 
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <div>Hello World</div>,
-  },
-]);
 
-createRoot(document.getElementById("root")).render(
+
+createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />,
-  </StrictMode>
-);
+     <RouterProvider router={router} />
+  </StrictMode>,
+)
